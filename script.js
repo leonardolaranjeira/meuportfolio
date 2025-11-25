e0d1a45121a6c8db1955ce685c2b463b3b4b2434
Leonardo Laranjeira <leonardo02lfc@gmail.com>
2025-11-24 23:14:19 -0300

    Adição de seção
    Novas informações e seção de certificados.

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 
+const barra = document.getElementById('bars_menu')
+const tema = document.getElementById('botao_tema')
 
+// Menu escondido para celular
+
+
+/*
+ - Contar os slides (.slide).
+
+ - Criar as bolinhas (indicadores) e marcar a bolinha active.
+
+ - Mudar o transform: translateX() do .slider-track quando as setas ou bolinhas forem clicadas.
+ */
